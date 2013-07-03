95e4fc045bf37b6af49f5f7bf6cbb1c5518bd44e
Robert Illner <captainchaos91@googlemail.com>
2013-07-03 09:12:20 +0200

    added a few methods.

--- src/vector/Vector.java
+++ src/vector/Vector.java
@@ -23,16 +23,17 @@ public class Vector {
 	}
 	
 	/**
-	 * Constructs a vector with the given length.
+	 * Constructs a vector with the given length. The given length has to be equal to or greather than zero.
 	 * @param length - length of the new vector.
+	 * @throws NegativeArraySizeException
 	 */
-	public Vector (int length) {
+	public Vector (int length) throws NegativeArraySizeException {
 		try {
 			len = length;
 			vec = new double [length];
 		}
 		catch (NegativeArraySizeException e) {
-			System.out.println("ERROR: invalid length parameter. length has to greather than -1.");
+			System.out.println("EXCEPTION: invalid length parameter. length has to greather than -1.");
 		}
 		
 	}
@@ -93,7 +94,7 @@ public class Vector {
 	}
 	
 	/**
-	 * Adds the given vector to the calling vector.
+	 * Adds the given vector to the calling vector. The length of these vectors has to be equal.
 	 * @param vec - the vector to add to the calling vector.
 	 * @return - the result of the addition as a new Vector-Object.
 	 * @throws ArrayIndexOutOfBoundsException
@@ -108,13 +109,13 @@ public class Vector {
 		}
 		else
 		{
-			throw new ArrayIndexOutOfBoundsException("ERROR: Can't add those vectors because their length isn't equal! ");
+			throw new ArrayIndexOutOfBoundsException("EXCEPTION: Can't add those vectors because their length isn't equal!");
 		}
 		return result;
 	}
 	
 	/**
-	 * Subtracts the given vector from the calling vector.
+	 * Subtracts the given vector from the calling vector. The length of these vectors has to be equal.
 	 * @param vec - the vector to subtract from the calling vector.
 	 * @return - the result of the subtraction as a new Vector-Object.
 	 * @throws ArrayIndexOutOfBoundsException
@@ -129,7 +130,7 @@ public class Vector {
 		}
 		else
 		{
-			throw new ArrayIndexOutOfBoundsException("ERROR: Can't sub those vectors because their length isn't equal! ");
+			throw new ArrayIndexOutOfBoundsException("EXCEPTION: Can't substract those vectors because their length isn't equal!");
 		}
 		return result;
 	}
@@ -163,5 +164,81 @@ public class Vector {
 			sum += Math.pow(this.getAt(i), 2);
 		}
 		return Math.sqrt(sum);
+	}
+	
+	/**
+	 * Multiplies the calling vector with the given scalar.
+	 * @param scalar - the scalar the calling vector will be multiplied with.
+	 * @return - the result vector.
+	 */
+	public Vector multWithScalar(double scalar) {
+		Vector result = new Vector(this.getLength());
+		for (int i = 0; i < this.getLength(); i++) {
+			result.setAt(i, this.getAt(i) * scalar);
+		}
+		return result;
+	}
+	
+	/**
+	 * Gets the dot product of the calling vector and the given vector. The length of these vectors has to be equal.
+	 * @param vec - the second vector for the dot product.
+	 * @return - the result as a double value.
+	 * @throws ArrayIndexOutOfBoundsException
+	 */
+	public double getdotProduct(Vector vec) throws ArrayIndexOutOfBoundsException {
+		double result;
+		if (this.getLength() == vec.getLength()) {
+			result = 0.0;
+			for (int i = 0; i < this.getLength(); i++) {
+				result += this.getAt(i) * vec.getAt(i);
+			}
+		}
+		else {
+			throw new ArrayIndexOutOfBoundsException("EXCEPTION: The length of the given vector has to be equal to the length of the calling vector!");
+		}
+		return result;
+	}
+	
+	/**
+	 * Gets the cross product of the calling vector and the given vector. The length of this vectors has to be 3.
+	 * @param vec - the second vector.
+	 * @return . the result as a new Vector-Object.
+	 * @throws ArithmeticException
+	 */
+	public Vector getCrossProduct(Vector vec) throws ArithmeticException {
+		Vector result = new Vector(3);
+		if ((this.getLength() == vec.getLength()) && (this.getLength() == 3)) {
+			result.setAt(0, (this.getAt(1) * vec.getAt(2)) - (this.getAt(2) * vec.getAt(1)));
+			result.setAt(1, (this.getAt(2) * vec.getAt(0)) - (this.getAt(0) * vec.getAt(2)));
+			result.setAt(2, (this.getAt(0) * vec.getAt(1)) - (this.getAt(1) * vec.getAt(0)));
+		}
+		else {
+			throw new ArithmeticException("EXCEPTION: The length of the given vector and the calling vector has to be 3!");
+		}
+		return result;
+	}
+	
+	/**
+	 * Proofs if the two given vectors are orthogonal of each other.
+	 * @param vec1 - first vector.
+	 * @param vec2 - second vector.
+	 * @return - true, if the vectors are orthogonal to each other. Otherwise false.
+	 */
+	public boolean areOrthogonal(Vector vec1, Vector vec2) {
+		if (vec1.getdotProduct(vec2) == 0)
+			return true;
+		else
+			return false;
+	}
+	
+	/**
+	 * Proofs if the calling vector is a unit vector.
+	 * @return -true, if the calling vector is a unit vector. Otherwise false.
+	 */
+	public boolean isUnitVector() {
+		if (this.getEuclideanNorm() == 0)
+			return true;
+		else
+			return false;
 	}
 }
--- src/vector/VectorTest.java
+++ src/vector/VectorTest.java
@@ -8,7 +8,13 @@ public class VectorTest {
 		a.setAt(1, 4);
 		a.setAt(2, 1);
 		a.setAt(3, 0);
-		System.out.println(a.getEuclideanNorm());
+		Vector b = new Vector(5);
+		b.setAt(0, 2);
+		b.setAt(1, -7);
+		b.setAt(2, 3);
+		b.setAt(3, 1);
+		b.setAt(4, -1);
+		System.out.println(a.getdotProduct(b));
 
 	}
 
